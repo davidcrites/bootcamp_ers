209fafad857fe386ceeb0c93d1998de351336d34
Crites <davidh.crites@capitalone.com>
2017-10-12 11:56:33 -0400

    testing

--- ers_project/src/main/java/com/capital/one/servlets/FrontControllerServlet.java
+++ ers_project/src/main/java/com/capital/one/servlets/FrontControllerServlet.java
@@ -8,6 +8,11 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.capital.one.datamodelbeans.Users;
+
+
 
 public class FrontControllerServlet extends HttpServlet{
 
@@ -15,17 +20,46 @@ public class FrontControllerServlet extends HttpServlet{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	
+	@Override
+	public void init() throws ServletException {
+		// TODO Auto-generated method stub
+		super.init();
+//		ServletConfig config = getServletConfig();
+//		System.out.println(config.getInitParameter("param1"));
+//		ServletContext context = getServletContext();
+//		System.out.println("servlet context param1: " + context.getInitParameter("param1"));
+	}
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		//super.doGet(req, resp);
+		
 		System.out.println("Processing Front Controller Get Request.");
-		ServletConfig config = getServletConfig();
-		System.out.println(config.getInitParameter("param1"));
-		ServletContext context = getServletContext();
-		System.out.println("servlet context param1: " + context.getInitParameter("param1"));
+		super.doGet(req, resp);
+		
+		//below only works with the user bean defined...casting to User because only know it is an object
+//		System.out.println((User)req.getSession().getAttribute("user"));
+		
+
 	}
 	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//super.doPost(req, resp);
+		
+		//below was a test and only works if you had a user bean defined with the login method to store the user w/attributes
+//		HttpSession sess = req.getSession();
+//		if (sess.getAttribute("user")!=null)){
+//		User user = us.login("blake", "pass");
+//			if(user != null) {
+//				sess.setAttribute("user", user);
+//			}
+//		}
+		
+	}
 
 
 }
