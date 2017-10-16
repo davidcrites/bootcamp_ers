4be0ef02f73b1eebc886a9c5d2362d6fe956b0be
Crites <davidh.crites@capitalone.com>
2017-10-15 21:48:16 -0400

    working on adding reimb services

--- ers_project/src/main/java/com/capital/one/services/ReimbursementService.java
+++ ers_project/src/main/java/com/capital/one/services/ReimbursementService.java
@@ -2,6 +2,7 @@ package com.capital.one.services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -25,10 +26,52 @@ public class ReimbursementService {
 	public void myPendingReimbursements(HttpServletRequest req, HttpServletResponse resp) {
 		List<Reimbursement> displayList = new ArrayList<Reimbursement>();
 		currentUser = (Users) req.getSession().getAttribute("currentUser");
-		
+		//false parameter in below call gets pending tickets
 		displayList = empDao.viewEmployeeTickets(currentUser.getErsUsersId(), false);
 		req.getSession().setAttribute("myPending", displayList);
-		System.out.println(displayList);
+		for(int i=0; i<displayList.size();i++) {
+			System.out.println(displayList.get(i));
+			System.out.println("");
+		}
+	}
+	
+	public void myPastReimbursements(HttpServletRequest req, HttpServletResponse resp) {
+		List<Reimbursement> displayList = new ArrayList<Reimbursement>();
+		currentUser = (Users) req.getSession().getAttribute("currentUser");
+		// true paramater in below call gets past tickets
+		displayList = empDao.viewEmployeeTickets(currentUser.getErsUsersId(), true);
+		req.getSession().setAttribute("myPending", displayList);
+		for(int i=0; i<displayList.size();i++) {
+			System.out.println(displayList.get(i));
+			System.out.println("");
+		}
+	}
+	
+	public void allPendingReimbursements(HttpServletRequest req, HttpServletResponse resp) {
+		List<Reimbursement> displayList = new ArrayList<Reimbursement>();
+		displayList = finManDao.getAllEmployeeReimbursement().stream()
+				.filter(element->"Pending".equals(element.getStatus().getReimbStatus()))
+				.collect(Collectors.toList());
+		req.getSession().setAttribute("allPending", displayList);
+		for(int i=0; i<displayList.size();i++) {
+			System.out.println(displayList.get(i));
+			System.out.println("");
+		}
+	}
+	
+	public void allPastReimbursements(HttpServletRequest req, HttpServletResponse resp) {
+		List<Reimbursement> displayList = new ArrayList<Reimbursement>();
+		log.trace("In rs.allPastReimbursements");
+		displayList = finManDao.getAllEmployeeReimbursement().stream()
+				.filter(element->{ return ("Approved".equals(element.getStatus().getReimbStatus()) ||
+										  "Denied".equals(element.getStatus().getReimbStatus()));})
+				.collect(Collectors.toList());
+		req.getSession().setAttribute("allPast", displayList);
+		log.trace("Geting ready to display the filtered displayList");
+		for(int i=0; i<displayList.size();i++) {
+			System.out.println(displayList.get(i));
+			System.out.println("");
+		}
 	}
 
 }
